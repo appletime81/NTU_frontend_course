a881b84e050adce09ec3b13ce7e9e03731630a5e
appletime81 <itrimsl8696@gmail.com>
2021-10-25 21:01:16 +0800

    Update

--- 台大前端課程/css-basic-example-master/js/main.js
+++ 台大前端課程/css-basic-example-master/js/main.js
@@ -1,1 +1,14 @@
 // 在這裡寫入你的JS邏輯
+// 小駝峰命名法: 第一個字全小寫，之後的單字首字大寫
+// ex. signInWithGoogle()
+
+// 選到.navbar-toggler，並為其綁定事件
+// jQuery() : 選需要控制的元素
+jQuery('.navbar-toggler').click(function(){
+    // 選擇目標被點擊後要做的事...
+    console.log('the btn is clicked')
+    // 選.navbar-list並新增activate分類
+    // addClass 新增分類
+    // toggloeClass 切換分類 
+    jQuery('.navbar-list').toggleClass('active')
+})
